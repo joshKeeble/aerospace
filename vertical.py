4356761d8f49c7928252f7ca40e28bfa6cdb11a5
joshKeeble <37347480+joshKeeble@users.noreply.github.com>
2018-06-08 14:25:33 -0700

    Add files via upload
    VERTICAL FINAL

--- vertical.py
+++ vertical.py
@@ -1,4 +1,6 @@
-def can_avoid_vertically(start, obstacle, height_limit=100):
+from math import sqrt
+import math
+def can_avoid_vertically(start, obstacle, drone_gps, obstacle_gps, waypoint_next_gps, height_limit=100):
     """
 
     Args:
@@ -7,13 +9,33 @@ def can_avoid_vertically(start, obstacle, height_limit=100):
         height_limit (int): speed_height is the ratio of speed the drone
             is moving to the height it can feasibly travel (height/distance)
 
+        drone/obstacle/waypoint_next _gps (list) are given in feet list x,y
+
     Returns:
         bool
 
     Examples:
-        can_avoid_vertically(start=400, obstacle=[300, 670], height_limit=60)
+        can_avoid_vertically(start=400, obstacle=[300, 670], height_limit=100)
     """
-    return True if (height_limit > start - obstacle[0]) or (height_limit > start + obstacle[1]) else False
+    #convert gps coords to feet, find out the offset of the drone from the obstacle then run check based off of new height
+
+    #compare gps of waypoint and drone to find the slope l
+    dist_here_to_wp = math.fabs((waypoint_next_gps[1] - drone_gps[1])) / math.fabs((waypoint_next_gps[0] - drone_gps[0]))
+    eqA = drone_gps[1] - waypoint_next_gps[1]
+    eqB = waypoint_next_gps[0] - drone_gps[0]
+    eqC = (drone_gps[0] * waypoint_next_gps[1]) - (waypoint_next_gps[0] * drone_gps[1])
+    d = (math.fabs((eqA*drone_gps[0])+(eqB*drone_gps[1])+eqC))/sqrt((eqA*eqA)+(eqB*eqB))
+
+
+
+    radius = obstacle[1] - obstacle[0]
+    z = sqrt((radius * radius)-(d*d))
+    #midPoint = radius
+    #initA = (start - radius) * (start - radius)
+    #initC  = (radius*radius)
+    #spliceRadius = sqrt(initC-initA)
+
+    return True if (height_limit > start - z) or (height_limit > start + z) else False #changed obstacle[0] and [1]
 
 
 def dodge_vertically():
